300cf06beeec1e0bc4e8599eef1bf624edbdaee9
Arushi Bajpai <connectarushii@gmail.com>
2019-09-14 18:40:36 +0530

    Sass Loaded added
    Sass Loaded added

--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,12 @@ module.exports = {
     module: {
         rules: [
           {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
+            test: /\.scss$/i,
+            use: ['style-loader', 'css-loader', 'sass-loader'],
             // css loader takes css and converts it to javascript
             // style loader takes that javascript and injects into dom
             // both used together have to be put in correct order
+            // order of pipe is R to L
           },
         ],
     }
