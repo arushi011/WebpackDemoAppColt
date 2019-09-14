95d45f8ad25ea325f8ea7fa91a8f8fae01bd3690
Arushi Bajpai <connectarushii@gmail.com>
2019-09-14 18:27:24 +0530

    css and style loaders added
    css and style loaders added

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,16 @@ module.exports = {
     output: {
         filename: "main.js",
         path:  path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+            // css loader takes css and converts it to javascript
+            // style loader takes that javascript and injects into dom
+            // both used together have to be put in correct order
+          },
+        ],
     }
 }
