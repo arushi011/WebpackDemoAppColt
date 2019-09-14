f95d2da15ae09532369dcc92f41e9522ef2edb07
Arushi Bajpai <connectarushii@gmail.com>
2019-09-15 00:37:32 +0530

    clean webpack plugin added to delete prod build older file for every new build

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,11 +2,14 @@ const path = require('path')
 const common = require('./webpack.common')
 const merge = require('webpack-merge')
 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = merge(common, {
     mode: "production",
     //devtool: "none", // to avoid evals
     output: {
         filename: "main.[ContentHash].js",
         path:  path.resolve(__dirname, "dist")
-    }
+    },
+    plugins: [ new CleanWebpackPlugin()]
 })
